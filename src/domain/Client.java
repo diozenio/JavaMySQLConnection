0d26b7d9d5e65df86a5718ed68470393c3350f97
diozenio <d.dionisiobsl@gmail.com>
2025-02-13 23:52:20 -0300

    Refactor Client class to inherit from new abstract Person class

--- src/domain/Client.java
+++ src/domain/Client.java
@@ -1,48 +1,8 @@
 package domain;
 
-public class Client {
-    private String cpf;
-    private String name;
-    private String address;
-    private String phone;
-
+public class Client extends Person {
     public Client(String cpf, String name, String address, String phone) {
-        this.cpf = cpf;
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
+        super(cpf, name, address, phone);
     }
 
     @Override
@@ -55,12 +15,8 @@ public class Client {
                 '}';
     }
 
+    @Override
     public void print() {
-        System.out.println("---------- Dados do Cliente ----------");
-        System.out.println("CPF: " + cpf);
-        System.out.println("Nome: " + name);
-        System.out.println("Endereço: " + address);
-        System.out.println("Telefone: " + phone);
-        System.out.println("------------------------------------");
+        System.out.println("CPF: " + cpf + " | Nome: " + name + " | Endereço: " + address + " | Telefone: " + phone);
     }
 } 
---
+++ src/domain/Person.java
@@ -0,0 +1,49 @@
+package domain;
+
+public abstract class Person {
+    protected String cpf;
+    protected String name;
+    protected String address;
+    protected String phone;
+
+    public Person(String cpf, String name, String address, String phone) {
+        this.cpf = cpf;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public abstract void print();
+} 
